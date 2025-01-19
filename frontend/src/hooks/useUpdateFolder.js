29a1bcb6bbe475028ac1532764e11dd2ec3f0271
hyosus <shaniceyeong@gmail.com>
2025-01-19 19:30:41 +0800

    invalidate queries

--- frontend/src/hooks/useUpdateFolder.js
+++ frontend/src/hooks/useUpdateFolder.js
@@ -6,6 +6,9 @@ import { useMutation } from "@tanstack/react-query";
 const useUpdateFolder = () => {
   const { mutate, ...rest } = useMutation({
     mutationFn: (data) => updateFolder(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries([FOLDER]);
+    },
   });
 
   return { updateFolder: mutate, ...rest };
--- frontend/src/hooks/useUpdateJournal.js
+++ frontend/src/hooks/useUpdateJournal.js
@@ -1,9 +1,15 @@
 import { updateEntry } from "@/lib/api";
 import { useMutation } from "@tanstack/react-query";
+import { JOURNAL } from "./useJournalById";
+import { JOURNALS_IN_FOLDER } from "./useJournalsInFolder";
 
 const useUpdateJournal = () => {
   const { mutate, ...rest } = useMutation({
     mutationFn: (data) => updateEntry(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries([JOURNAL]);
+      queryClient.invalidateQueries([JOURNALS_IN_FOLDER]);
+    },
   });
 
   return { updateJournal: mutate, ...rest };
